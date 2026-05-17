1a2c2171e648ee1fad66b370524c0267c03b79b2
Aamil Qureshi <227041267+AamilTechnologies@users.noreply.github.com>
2026-05-17 20:39:44 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,40 @@
 function drawPlayer(){
 
+/* ===== PLAYER IMAGE ===== */
+
+const playerImg = new Image();
+
+playerImg.src =
+"https://i.imgur.com/9Xn4F6M.png";
+
+/* ===== DRAW PLAYER ===== */
+
+function drawPlayer(){
+
 ctx.save();
 
-ctx.shadowBlur = 35;
-ctx.shadowColor = "#00d4ff";
+ctx.shadowBlur = 25;
+
+ctx.shadowColor =
+"#00d4ff";
+
+/* DRAW CHARACTER */
+
+ctx.drawImage(
+
+playerImg,
+
+player.x,
+player.y,
+
+80,
+80
+
+);
+
+ctx.restore();
+
+}
 
 /* BODY */
 
